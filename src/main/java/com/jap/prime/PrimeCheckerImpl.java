4d96b33bbcdd12de77f073cc90e6e33502050338
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-09-05 15:57:40 +0530

    Created the PrimeCheckerImpl class Written the logic to check the number is prime with anonymous and lamda block

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -3,7 +3,51 @@ package com.jap.prime;
 public class PrimeCheckerImpl {
     public boolean primeNumberCheck(int number) {
         // write the logic here to check is a number is prime or not
-        return false;
+
+        /*PrimeChecker primeChecker = new PrimeChecker() {
+            public boolean primeNumberChecking(int number) {
+               int count =0;
+                boolean flag = false;
+                if (number == 0 || number == 1){
+                    return flag;
+                }
+                for (int i = 2; i <number; i++) {
+                    if(number%i == 0){
+                        count++;
+                    }
+                }
+                if (count == 0){
+                   flag = true;
+                }
+
+               return flag;
+            }
+        };
+        return primeChecker.primeNumberChecking(number);*/
+
+        PrimeChecker primeChecker = number1 -> {
+            int count =0;
+            boolean flag = false;
+            if (number == 0 || number == 1){
+                return flag;
+            }
+            for (int i = 2; i <number; i++) {
+                if(number%i == 0){
+                    count++;
+                }
+            }
+            if (count == 0){
+                flag = true;
+            }
+
+            return flag;
+
+
+        };
+
+
+
+       return primeChecker.primeNumberChecking(number);
     }
 
 }
